9474114977497e398a691dd1b974b5341f53915e
jnavas98 <j.navasdam@outlook.es>
2023-11-05 08:16:21 +0100

    Update main.ts

--- back/main.ts
+++ back/main.ts
@@ -1,12 +1,12 @@
 import express from "npm:express@4.18.2";
 
 const app = express();
-
+const env = await load();
 app.get('/', (req, res) => {
     console.log("Peticion recibida");
     res.send("¡Hola, mundo!");
 });
-const PORT=process.env.PORT||4000;
+const PORT=process.env.PORT||3000;
 app.listen(4000, () => {
     console.log("Corriendo en el puerto 4000");
 });
