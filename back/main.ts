3e8004f83574a20c4406d789683f2151b06dfec0
jnavas98 <j.navasdam@outlook.es>
2023-11-05 08:02:22 +0100

    App

---
+++ back/main.ts
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get('/', (req, res) => {
+    console.log("Peticion recibida");
+    res.send("¡Hola, mundo!"); // Envía una respuesta al cliente
+});
+
+app.listen(4000, () => {
+    console.log("Corriendo en el puerto 4000");
+});
