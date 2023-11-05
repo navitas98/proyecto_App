94d20e9486f20fa5d961166aa092a9de2f84b18c
jnavas98 <j.navasdam@outlook.es>
2023-11-05 08:29:52 +0100

    as

--- back/main.ts
+++ back/main.ts
@@ -1,14 +1,15 @@
 import express from "npm:express@4.18.2";
 import { load } from "https://deno.land/std@0.204.0/dotenv/mod.ts";
+require('dotenv').config()
 const app = express();
-const PORT=process.env.PORT||3000;
+const PORT = Deno.env.get("PORT");
 
 app.get('/', (req, res) => {
     console.log("Peticion recibida");
     res.send(PORT);
 });
 
-
+ 
 app.listen(4000, () => {
     console.log("Corriendo en el puerto 4000");
 });
