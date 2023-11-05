73affa5b4f0b8fdc479e91745afaea8d2a585399
jnavas98 <j.navasdam@outlook.es>
2023-11-05 08:14:06 +0100

    as

--- back/main.ts
+++ back/main.ts
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
     console.log("Peticion recibida");
     res.send("¡Hola, mundo!");
 });
-
+const PORT=process.env.PORT||4000;
 app.listen(4000, () => {
     console.log("Corriendo en el puerto 4000");
 });
