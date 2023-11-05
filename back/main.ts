d644fd7c487e103193e3644879cc02c71f218b57
jnavas98 <j.navasdam@outlook.es>
2023-11-05 09:34:51 +0100

    archivo html
    Creamos un archivo html con hola mundo

--- back/main.ts
+++ back/main.ts
@@ -1,6 +1,7 @@
 import express from "npm:express@4.17.1";
 import mongoose from "npm:mongoose@8.0.0";
 import { load } from "https://deno.land/std@0.204.0/dotenv/mod.ts";
+import path from "path";
 const env = await load();
 const PORT = env.PORT;
 if(!PORT){
@@ -22,12 +23,13 @@ if(!PORT){
 */// create express app
 const app = express();
 app.use(express.json());
-
+app.use(express.static(path.join(__dirname,"public")));
+/*
 // create endpoints
 app
  .get('/',(req,res)=>{
     res.status(200).send("conectado")
- })
+ })*/
 
 // start express server
 app.listen(PORT, () => {
