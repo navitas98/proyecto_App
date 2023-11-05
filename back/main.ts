d8124af0dc1b55473b523701839acbda9c7bf60c
jnavas98 <j.navasdam@outlook.es>
2023-11-05 09:14:22 +0100

    ENC
    Accediendo al archivo .env

--- back/main.ts
+++ back/main.ts
@@ -1,15 +1,37 @@
 import express from "npm:express@4.17.1";
+import mongoose from "npm:mongoose@8.0.0";
 import { load } from "https://deno.land/std@0.204.0/dotenv/mod.ts";
-const PORT = Deno.env.get("PORT");
+const env = await load();
+const PORT = env.PORT;
+if(!PORT){
+    console.log("No existe PORT")
+}
+//const DB = Deno.env.get("DB") || "contactos";
+
+/*if (!MONGO_URL) {
+  throw new Error("Please provide a MONGO_URL");
+  Deno.exit(-1);
+}
+
+// connecto to MongoDB
+
+//const uri = `${MONGO_URL}/${DB}?retryWrites=true&w=majority`;
+//await mongoose.connect(uri);
+//console.info("MongoDB connected");
+
+*/// create express app
 const app = express();
-
-
-app.get('/', (req, res) => {
-    console.log("Peticion recibida");
-    res.send("conectado");
-});
-
-
-app.listen(4000, () => {
-    console.log("Corriendo en el puerto 4000");
+app.use(express.json());
+/*
+// create endpoints
+app
+  .get("/api/contactos", getAllContacts)
+  .get("/api/contactos/:dni", getContactFromDNI)
+  .post("/api/contactos", pushContact)
+  .put("/api/contactos/:dni", putContact)
+  .delete("/api/contactos/:dni", deleteContact);
+*/
+// start express server
+app.listen(PORT, () => {
+  console.info(`Server started on port  ${PORT}`);
 });
